748b0a63fbd4939efdca18dcfe657002d7d3bb7b
Enoch Phetteplace <curiousaps@gmail.com>
2024-04-29 11:45:52 -0500

    Add my dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,14 +1,31 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+
+    id("kotlin-kapt")
+
+    id("kotlin-parcelize")
+    id("com.google.dagger.hilt.android")
+    id("com.google.devtools.ksp")
 }
 
+
+kotlin {
+    sourceSets {
+        debug {
+            kotlin.srcDir("build/generated/ksp/debug/kotlin")
+        }
+        release {
+            kotlin.srcDir("build/generated/ksp/release/kotlin")
+        }
+    }
+}
 android {
-    namespace = "com.curiousapps.tomlsetup"
+    namespace = "com.curiousapps.rocketgame"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.curiousapps.tomlsetup"
+        applicationId = "com.curiousapps.rocketgame"
         minSdk = 28
         targetSdk = 34
         versionCode = 1
@@ -30,11 +47,12 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        isCoreLibraryDesugaringEnabled = true
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
@@ -59,6 +77,8 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -66,4 +86,55 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+    coreLibraryDesugaring ("com.android.tools:desugar_jdk_libs:1.1.5")
+
+    // OpenCSV
+    implementation ("com.opencsv:opencsv:5.5.2")
+
+    // Compose dependencies
+    implementation ("androidx.compose.material:material-icons-extended:1.5.1")
+    implementation ("com.google.accompanist:accompanist-flowlayout:0.17.0")
+    implementation ("com.google.accompanist:accompanist-swiperefresh:0.24.2-alpha")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
+
+    // Compose Nav Destinations
+    val raam_costa_version = "1.11.2-alpha"
+    implementation ("io.github.raamcosta.compose-destinations:core:$raam_costa_version")
+    ksp ("io.github.raamcosta.compose-destinations:ksp:$raam_costa_version")
+
+    // Coil Compose
+    implementation("io.coil-kt:coil-compose:2.2.2")
+
+    // Dagger - Hilt
+    val hilt_version = "2.51"
+    implementation("com.google.dagger:hilt-android:$hilt_version")
+    kapt("com.google.dagger:hilt-android-compiler:$hilt_version")
+    kapt("androidx.hilt:hilt-compiler:1.0.0")
+    implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
+    //implementation ("androidx.hilt:hilt-lifecycle-viewmodel:1.0.0-alpha03")
+
+    // Paging
+    implementation("androidx.paging:paging-runtime-ktx:3.1.1")
+    implementation("androidx.paging:paging-compose:1.0.0-alpha18")
+
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("com.squareup.okhttp3:okhttp:5.0.0-alpha.3")
+    implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.3")
+
+    // Room
+
+    implementation ("org.xerial:sqlite-jdbc:3.34.0")
+    val room_version = "2.6.1"
+    implementation("androidx.room:room-runtime:$room_version")
+    annotationProcessor("androidx.room:room-compiler:$room_version")
+    implementation("androidx.room:room-ktx:$room_version")
+    ksp("androidx.room:room-compiler:$room_version")
+}
+
+kapt{
+    correctErrorTypes = true
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,20 @@
 plugins {
     id("com.android.application") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("com.android.library") version "8.2.2" apply false
+    id("com.google.devtools.ksp") version "1.9.21-1.0.16" apply false
+    id("com.google.dagger.hilt.android") version "2.51" apply false
+}
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        val nav_version = "2.7.6"
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$nav_version")
+
+        classpath ("com.google.dagger:hilt-android-gradle-plugin:2.51")
+    }
 }
